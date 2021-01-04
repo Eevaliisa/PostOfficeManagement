47aaec1ef0debcd425487cdfa0ce7d94127f3b6e
eevaliisa <eevalr@gmail.com>
2021-01-04 09:48:59 +0200

    added formik, fixed getting id from shipmentlist in getletterbagslist

--- ClientApp/src/components/AddShipment.js
+++ ClientApp/src/components/AddShipment.js
@@ -1,100 +1,126 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from "yup";
+
+const AddShipmentSchema = Yup.object().shape({
+
+    shipmentId: Yup.string()
+        .matches(/^[0-9a-zA-Z]{3}\-[0-9a-zA-Z]{6}$/,
+            "Incorrect format. Please use format XXX-XXXXXX")
+        .required("Shipment Id is required"),    
+});
 
 export class AddShipment extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
             
-            shipmentId: "",
-            destinationAirport: "",
-            flightNumber: "",
-            flightDateTime: "",
+    //         shipmentId: "",
+    //         destinationAirport: "",
+    //         flightNumber: "",
+    //         flightDateTime: "",
 
-        };    
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    //     };    
+        //this.handleInputChange = this.handleInputChange.bind(this);
+        //this.handleSubmit = this.handleSubmit.bind(this);
+    // }
 
-    handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value
-        });
-    }
+    // handleInputChange = (event) => {
+    //     const target = event.target;
+    //     const value = target.value;
+    //     const name = target.name;
+    //     this.setState({
+    //         [name]: value
+    //     });
+    // }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+    // handleSubmit = (event) => {
+    //     event.preventDefault();
         
-        fetch('http://localhost:5000/api/shipment', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify(this.state)
-          }).then((response) => {
-            console.log(response)
-            return response.json();
-          });
-        console.log(this.state)
-        
-    }
+    //     fetch('http://localhost:5000/api/shipment', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //           },
+    //         body: JSON.stringify(this.state)
+    //       }).then((response) => {
+    //         console.log(response)
+    //         return response.json();
+    //       });
+    //     console.log(this.state);   
+    // }
 
     render() {
         return (
             <div>
                 <h2>Add New Shipment</h2>
 
-                    <form onSubmit={this.handleSubmit}>
+                <Formik
+                    initialValues={{shipmentId: "", destinationAirport: "", flightNumber: "", flightDateTime: ""}}
+                    validationSchema={AddShipmentSchema}
+                    onSubmit={(values, { setSubmitting }) => {
+                        console.log('submitting')
+                        fetch('http://localhost:5000/api/shipment', {
+                            method: 'POST',
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(values)
+                            }).then((response) => {
+                                console.log(response)
+                                return response.json();
+                            });
+                            console.log(this.state);
+                            console.log('submitted')
+
+                     setSubmitting(false);
+                    }}
+                >
+                {({ errors, touched }) => (
+                    <Form>
                     <div className="form-group">
-                        <label htmlFor="shipment-id">Shipment ID</label>
-                        <input 
-                            type="text" 
-                            name="shipmentId"
-                            onChange={this.handleInputChange} 
-                            value = {this.props.shipmentId} 
+                        <label htmlFor="shipmentId">Shipment ID</label>
+                        <Field name="shipmentId" 
                             className="form-control" 
-                            id="shipment-id" 
                             placeholder="Use following the format: XXX-XXXXXX"/>
+                            {errors.shipmentId && touched.shipmentId ? (
+                            <div>{errors.shipmentId}</div>
+                            ) : null}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="airport">Destination Airport</label>
-                        <input 
+                        <label htmlFor="destinationAirport">Destination Airport</label>
+                        <Field 
                             type="text" 
-                            name="destinationAirport"
-                            onChange={this.handleInputChange} 
-                            value = {this.props.destinationAirport} 
+                            name="destinationAirport" 
                             className="form-control" 
-                            id="airport" 
+                            id="destinationAirport" 
                             placeholder="possible values are TLL, RIX or HEL"/>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="flight-number">Flight Number</label>
-                        <input 
+                        <label htmlFor="flightNumber">Flight Number</label>
+                        <Field
                             type="text" 
-                            name="flightNumber"
-                            onChange={this.handleInputChange} 
-                            value = {this.props.flightNumber} 
+                            name="flightNumber" 
                             className="form-control" 
-                            id="flight-number" 
+                            id="flightNumber" 
                             placeholder="Use format LLNNNN where L - letter, N - number"/>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="flight-date">Flight Number</label>
-                        <input 
+                        <label htmlFor="flightDateTime">Flight Date</label>
+                        <Field 
                             type="text" 
-                            name="flightDateTime"
-                            onChange={this.handleInputChange} 
-                            value = {this.props.flightDateTime} 
+                            name="flightDateTime" 
                             className="form-control" 
-                            id="flight-date" 
+                            id="flightDateTime" 
                             placeholder="Use format YYYY-MM-DD"/>
                     </div>
                     <button className="btn btn-primary" type="submit">Add Shipment</button>
-                    </form>
+                    </Form>
+                    )}
+                </Formik>
             </div>
         )
     }
